b3f1d7480f7b8e5352451edb0f28bda1ee82644b
Joana Nushi <joana.nushi29@gmail.com>
2023-10-01 16:47:39 +0200

    final

--- static/snake_game.js
+++ static/snake_game.js
@@ -2,11 +2,7 @@ const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
 function drawGame(gameState) {
-    // Implement the code to draw the game on the canvas using the gameState
-    // You'll need to translate your Python code to JavaScript here
-    // ...
 
-    // Use setTimeout or requestAnimationFrame to periodically call drawGame
     setTimeout(() => {
         fetch('/get_game_state')
             .then(response => response.json())
@@ -16,7 +12,6 @@ function drawGame(gameState) {
     }, 100);
 }
 
-// Start the game by fetching the initial game state
 fetch('/get_game_state')
     .then(response => response.json())
     .then(data => {
